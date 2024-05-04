2a594004a20982a1d971604de9346826fc0a0d60
Ertunga Yusuf OCAK <ocakertungayusuf@gmail.com>
2024-05-04 18:31:16 +0300

    user server kısmı çalışan 1

--- user.py
+++ user.py
@@ -1,47 +1,47 @@
 import socket
 import threading
 
-header_lenght = 20
-user_ip = socket.gethostbyname(socket.gethostname()) #kullanıcının kendi ip'si
-contacted_ip = "192.168.1.36"
-port = 5050
-tower = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connetion_address = user_ip, port
-tower.bind(connetion_address)
-tower.connect((contacted_ip,port))
+#bağlantı portu:
+port = 9080
 
+#karşıdakinin ip'si:
+contacted_ip = input("Karşıdakinin ip adresi : ")
 
-def handle_with_connection(conn, addr):
-    while True:
-        message_length = conn.recv(header_lenght).decode('utf-8')
-        if message_length:
-            message = conn.recv(int(message_length)).decode('utf-8')
-            print(f"[{addr}]: {message}")
-            #tower.send("message recieved".encode('utf-8'))
-            if message == "end":
-                print(f"{addr} ended connection ")
-                break
+#kendi ip'miz:
+user_ip = socket.gethostbyname(socket.gethostbyname())
+
+#alınan ilk mesaj 64 bytlık bir header, bu değişken uzunluğu tutuyor:
+header = 64
+disconnect_message = "#quit"
+#Her kullanıcı aynı zamanda bir server:
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ADDR = (contacted_ip,port)
+server.bind(ADDR)
+
+#gelen yeni bağlantıları kontrol eden fonksiyon:
+def handle_contact(conn,addr):
+    print(f"{addr} ip'li cihaz bağlandı")
+    connected = True
+    while connected:
+        #karşı taraftan mesaj bekler:
+        message_length = conn.recv(header).decode("utf-8")
+        message_length = int(message_length)
+        message = conn.recv(message_length).decode("utf-8")
+        if message == disconnect_message:
+            connected = False
+            print(f"{addr} ip'li cihazla bağlantı sonlandırıldı")
+        else:
+            print(f"[{addr} ip'li cihaz]: {message}")
 
     conn.close()
-def send_message(message):
-    prepared_message = message.encode('utf-8')
-    leght_for_header = len(prepared_message)
-    prepared_lenght_header = str(leght_for_header).encode('utf-8')
-    prepared_lenght_header += b' '*(header_lenght-len(prepared_lenght_header))
-    tower.send(prepared_lenght_header)
-    tower.send(prepared_message)
 
-tower.listen()
-print(f"{user_ip} ip'li cihaz gelen bağlantıları dinliyor... ")
-while True:
-    conn, addr = tower.accept()
-    thread = threading.Thread(target=handle_with_connection, args=(conn, addr))
-    print(f"{addr} ip'li cihaz bağlandı ")
-    print(f"active connection number is {threading.active_count()}")
-    thread.start()
+def start():
+    print(f"{user_ip} ip'li cihaz gelen bağlantıları dinliyor...")
+    server.listen()
     while True:
-        message = input("what is your message: ")
-        send_message(message)
-        if message == "end":
-            break
+        conn,addr = server.accept()
+        thread = threading.Thread(target=handle_contact(), args=(conn,addr))
+        thread.start()
 
+
+start()
