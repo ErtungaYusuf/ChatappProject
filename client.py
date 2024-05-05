6d982b1fc92f55db714f54b1f55c6ddc228f622e
Ertunga Yusuf OCAK <ocakertungayusuf@gmail.com>
2024-05-05 20:44:02 +0300

    fernet şifreleme eklendi

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from cryptography.fernet import Fernet
 
 header = 64
 port = 5050
@@ -13,16 +14,21 @@ client.connect(ADDR)
 senden_messages = []
 recieved_messages = []
 
+#şifreleme:
+key = b'7a1vOxwC8XviL6IFcsCEo0xrQM_7_6A_kBz2e3qLmII='
+fernet = Fernet(key)
+
 def send():
     while True:
-        message = input("Mesajınız:")
+        message = input("[+]:")
         senden_messages.append(message)
         message_encoded = message.encode("utf-8")
-        message_length = len(message_encoded)
+        encMessage = fernet.encrypt(message_encoded)
+        message_length = len(encMessage)
         send_length = str(message_length).encode("utf-8")
         send_length += b' ' * (header - len(send_length))
         client.send(send_length)
-        client.send(message_encoded)
+        client.send(encMessage)
 
 def receive():
     while True:
@@ -30,8 +36,10 @@ def receive():
         if message_length:
             message_length = int(message_length)
             message = client.recv(message_length).decode("utf-8")
-            print(f"Server'dan mesaj: {message}")
-            recieved_messages.append(message)
+            decMessage = fernet.decrypt(message).decode()
+            recieved_messages.append(decMessage)
+            print(f"Server'dan mesaj: {decMessage}")
+
 
 
 def start():
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-
+from cryptography.fernet import Fernet
 
 port =5050
 #gerçek internette kullanımda bu ip cihazın public ip'sine eşitlenmeli
@@ -17,6 +17,11 @@ disconnect_message = "#quit"
 sended_messages = []
 recieved_messages = []
 
+
+#şifreleme:
+key = b'7a1vOxwC8XviL6IFcsCEo0xrQM_7_6A_kBz2e3qLmII='
+fernet = Fernet(key)
+
 def handle_contact(conn,addr):
     print(f"{addr} ip'li cihaz bağlandı")
     connected = True
@@ -26,24 +31,28 @@ def handle_contact(conn,addr):
         if message_length:
             message_length = int(message_length)
             message = conn.recv(message_length).decode("utf-8")
+            decMessage = fernet.decrypt(message).decode()
             if message == disconnect_message:
                 connected = False
                 print(f"{addr} ip'li cihazla bağlantı sonlandırıldı")
             else:
-                recieved_messages.append(message)
-                print(f"[{addr} ip'li cihaz]: {message}")
+
+                recieved_messages.append(decMessage)
+                print(f"[{addr} ip'li cihaz]: {decMessage}")
     conn.close()
 
 def handle_send_message(conn,addr):
     while True:
-        message = input("Mesajınız:")
+        message = input("[+]:")
         sended_messages.append(message)
         message_encoded = message.encode("utf-8")
-        message_length = len(message_encoded)
+        #şifreleme:
+        encMessage = fernet.encrypt(message_encoded)
+        message_length = len(encMessage)
         send_length = str(message_length).encode("utf-8")
         send_length += b' ' * (header - len(send_length))
         conn.send(send_length)
-        conn.send(message_encoded)
+        conn.send(encMessage)
 
 
 def start():
